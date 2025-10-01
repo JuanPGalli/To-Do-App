37de5b2f4003e6496c2beed0af2fac3c26d0ea66
Juan Pablo Galli <jpgallir@gmail.com>
2025-10-01 20:17:14 -0300

    feat: add onToggle & onDelete with execution and pass them as props to TaskItem

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,19 @@
 import TaskItem from './TaskItem';
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, onToggle, onDelete }) => {
   if (!tasks || tasks.length === 0) {
-    return <p className='empty'>No tienes tareas pendientes</p>;
+    return <p className='empty'>No tienes tareas pendientes 🎉</p>;
   }
 
   return (
     <ul className='task-list'>
       {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
+        <TaskItem
+          key={task.id}
+          task={task}
+          onToggle={() => onToggle(task.id)}
+          onDelete={() => onDelete(task.id)}
+        />
       ))}
     </ul>
   );
