e0c41f0870ee6820eaf00f2ef69dbd766968bad6
Juan Pablo Galli <jpgallir@gmail.com>
2025-10-01 17:03:39 -0300

    add TaskList functionality

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import TaskItem from './TaskItem';
 
-const TaskList = () => {
-  return <div>TaskList</div>;
+const TaskList = ({ tasks }) => {
+  if (!tasks || tasks.length === 0) {
+    return <p className='empty'>No tienes tareas pendientes</p>;
+  }
+
+  return (
+    <ul className='task-list'>
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </ul>
+  );
 };
 
 export default TaskList;
