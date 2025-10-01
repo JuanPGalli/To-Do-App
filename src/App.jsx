3c6cbab2cbd945b6752309cb50ff4b6bdda9f56d
Juan Pablo Galli <jpgallir@gmail.com>
2025-10-01 13:51:06 -0300

    Add Header Component plus Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './components/Header';
+
 const App = () => {
-  return <div className='app-container'></div>;
+  return (
+    <div className='app-container'>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className='header'>Mis Tareas &#x2705;</h1>
+    </div>
+  );
+};
+
+export default Header;
