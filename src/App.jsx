a20460c3a5b6d4023869e67d117c3303762d72ed
Juan Pablo Galli <jpgallir@gmail.com>
2025-10-01 17:21:44 -0300

    add addTask function and pass it through props when rendering TaskForm component. Render of TaskList component passing tasks by props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(text) {
+    const newTask = {
+      id: Date.now().toString() + Math.random().toString(36).slice(2),
+      text: text.trim(),
+      completed: false,
+    };
+    setTasks((prev) => [newTask, ...prev]);
+    return tasks;
+  }
+
   return (
     <div className='app-container'>
       <Header />
+      <div>
+        <TaskForm addTask={addTask} />
+        <TaskList tasks={tasks} />
+      </div>
     </div>
   );
 };
