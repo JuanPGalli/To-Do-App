a7cde66eaa9f15ea50e2fc45099f524b4182ecd9
Juan Pablo Galli <jpgallir@gmail.com>
2025-10-01 17:19:03 -0300

    add functionality to TaskItem to show the task list

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,5 +1,11 @@
-const TaskItem = () => {
-  return <div>TaskItem</div>;
+const TaskItem = ({ task }) => {
+  return (
+    <li>
+      <span className='task-text' aria-label={`Tarea: ${task.text}`}>
+        {task.text}
+      </span>
+    </li>
+  );
 };
 
 export default TaskItem;
